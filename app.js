842e6faada53cbb67705612e3344950f30e867d0
Iano-theDev <sinceianmike@gmail.com>
2023-01-23 00:05:29 +0300

    Add feature: Prevent empty submission

--- app.js
+++ app.js
@@ -13,40 +13,46 @@ filterOption.addEventListener('click', filterTodo);
 
 //Functions
 function addTodo(event) {
-    event.preventDefault();
-    // create div elelmet to display tasks to be done
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
 
-    //create single task item 
-    const newTodo = document.createElement('li');
-    newTodo.innerText = todoInput.value
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
-
-    // Add task to local storage
-    saveLocalTodos(todoInput.value)
-
-    //check button for completed tasks
-    const completedButton = document.createElement('button')
-    completedButton.innerHTML= '<i class="fa-solid fa-check"></i>';
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-
-    //undo a comleted task***************************************
-    const undoComplete = document.createElement('button');
-    undoComplete.innerHTML = '<i class="fa-solid fa-rotate-left"></i>';
-    undoComplete.classList.add("undo-button");
-    // todoDiv.appendChild(undoComplete);
-
-    //delete button to delete tasks
-    const trashButton = document.createElement('button')
-    trashButton.innerHTML= '<i class="fa-solid fa-trash"></i>';
-    trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
-
-    //append to list
-    todoList.appendChild(todoDiv);
+    //check if the user input is empty or not
+    if (todoInput.value.length > 0) {
+        event.preventDefault();
+        // create div elelmet to display tasks to be done
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+    
+        //create single task item 
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todoInput.value
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
+    
+        // Add task to local storage
+        saveLocalTodos(todoInput.value)
+    
+        //check button for completed tasks
+        const completedButton = document.createElement('button')
+        completedButton.innerHTML= '<i class="fa-solid fa-check"></i>';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+    
+        //undo a comleted task***************************************
+        const undoComplete = document.createElement('button');
+        undoComplete.innerHTML = '<i class="fa-solid fa-rotate-left"></i>';
+        undoComplete.classList.add("undo-button");
+        // todoDiv.appendChild(undoComplete);
+    
+        //delete button to delete tasks
+        const trashButton = document.createElement('button')
+        trashButton.innerHTML= '<i class="fa-solid fa-trash"></i>';
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+    
+        //append to list
+        todoList.appendChild(todoDiv);
+    } else {
+        console.log("Input required")
+    }
 
     //clear task input value
     todoInput.value = "";
@@ -61,6 +67,7 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         //add fall animation 
         todo.classList.add("fall")
+        removeLocalTodos(todo);
         todo.addEventListener('transitionend', function() {
             todo.remove();
         })
@@ -74,6 +81,8 @@ function deleteCheck(e) {
     }
 }
 
+
+// filter out completed, uncompleted and all tasks
 function filterTodo(e) {
     const todos = todoList.childNodes;
     todos.forEach(todo => {
@@ -115,8 +124,7 @@ function saveLocalTodos(todo) {
 }
 
 // retrive tasks from local storage
-function getTodos() {
-    
+function getTodos() { 
     let todos;
     if (localStorage.getItem('todos') === null){
         todos = [];
@@ -134,9 +142,6 @@ function getTodos() {
         newTodo.innerText = todo;
         newTodo.classList.add('todo-item');
         todoDiv.appendChild(newTodo);
-
-        // Add task to local storage
-        // saveLocalTodos(todoInput.value)
 
         //check button for completed tasks
         const completedButton = document.createElement('button')
@@ -157,3 +162,15 @@ function getTodos() {
 
 
 //Delete tasks from local storage
+function removeLocalTodos(todo) {
+    if (localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    //get the index of the todo
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
