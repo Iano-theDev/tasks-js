76497f55a5ea0c7b2abb2df7dfa255f6e3ba4a54
Iano-theDev <sinceianmike@gmail.com>
2023-01-25 11:58:25 +0300

    Update

--- test/test.js
+++ test/test.js
@@ -11,10 +11,20 @@
 
 // alert("hello")
 
-sayHello();
+// sayHello();
 
 
-function sayHello() {
-  var name = 'Ian';
-  return name;
-}
+// function sayHello() {
+//   var name = 'Ian';
+//   return name;
+// }
+
+let colors = ['red', 'blue'];
+colors[2] = 'brown';
+// console.log(colors)
+
+colors.unshift('yellow');
+let reverseColors = colors.reverse()
+let spliceColors = colors.splice(0, 1);
+let popYellow = colors.splice(2, 1)
+console.log(colors);
